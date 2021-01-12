5af4614b320644d12eaa667beab04f89cb089114
Math1299 <perret.mathieu@bluewin.ch>
2021-01-12 17:11:55 +0100

    nav-gauche OK avec JS

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const btnMenu = document.querySelector(".btn-round-menu");
 const nav = document.querySelector(".nav-gauche");
-const allItemNav = document.querySelectorAll(".nav-nemu-items");
+const allItemNav = document.querySelectorAll(".nav-menu-items");
 const line = document.querySelector(".container-line");
 
 btnMenu.addEventListener("click", () => {
   line.classList.toggle("active");
+  nav.classList.toggle("menu-visible");
 });
+
+if (window.matchMedia("(max-width: 1300px)")) {
+  allItemNav.forEach((items) => {
+    items.addEventListener("click", () => {
+      nav.classList.toggle("menu-visible");
+      line.classList.toggle("active");
+    });
+  });
+}

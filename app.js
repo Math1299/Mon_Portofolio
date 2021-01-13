ebda512fac7e86f47c4f47d3b158aec2f11e7f3c
Math1299 <perret.mathieu@bluewin.ch>
2021-01-13 16:41:52 +0100

    animation js accueil

--- app.js
+++ app.js
@@ -16,3 +16,32 @@ if (window.matchMedia("(max-width: 1300px)")) {
     });
   });
 }
+
+//ANIMATION ECRITURE
+
+const txtAnim = document.querySelector(".txt-animation");
+
+let typewriter = new Typewriter(txtAnim, {
+  loop: false,
+  deleteSpeed: 20,
+});
+
+typewriter
+  .pauseFor(1800)
+  .changeDelay(20)
+  .typeString("Moi c'est Mathieu Perret")
+  .pauseFor(300)
+  .typeString("<Strong>, Développeur Web</Strong> !")
+  .pauseFor(1000)
+  .deleteChars(5) //POUR DELETE DES CARACTERES
+  .typeString('<span style="color: #27ae60;"> CSS</span> !')
+  .pauseFor(1000)
+  .deleteChars(5) //POUR DELETE DES CARACTERES
+  .typeString('<span style="color: #EA39ff;"> PhP</span> !')
+  .pauseFor(1000)
+  .deleteChars(5) //POUR DELETE DES CARACTERES
+  .typeString('<span style="color: midnightblue;"> React</span> !')
+  .pauseFor(1000)
+  .deleteChars(8) //POUR DELETE DES CARACTERES
+  .typeString('<span style="color: #ff6910;"> JavaScript</span> !')
+  .start();
